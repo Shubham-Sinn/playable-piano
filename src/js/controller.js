9758c483f03cdd5e2b005f8221513ad82237b131
Shubham-Sinn <reachshubham001@gmail.com>
2024-02-20 10:38:44 +0530

    final commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,1 +1,18 @@
-console.log('connected');
+let pianoContainer = document.getElementsByClassName('piano-container');
+const base = './src/audio/';
+
+window.onload = () => {
+  for (let i = 1; i <= 24; i++) {
+    let div = document.createElement('div');
+    div.classList.add('key', i <= 10 ? 'black-key' : 'white-key');
+
+    // for playing audio on click
+    const number = i <= 9 ? '0' + i : i;
+    div.addEventListener('click', () => {
+      new Audio(`${base}key${number}.mp3`).play();
+    });
+
+    pianoContainer[0].appendChild(div);
+    console.log(div);
+  }
+};
